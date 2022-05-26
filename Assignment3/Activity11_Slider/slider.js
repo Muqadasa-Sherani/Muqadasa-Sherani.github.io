b221f999519859e140f658978a25b665702c2bc7
Muqadasa Sherani <97336771+Muqadasa-Sherani@users.noreply.github.com>
2022-05-26 22:33:54 +0300

    Add files via upload

---
+++ Assignment3/Activity11_Slider/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  randomStart: true,
+			  pager: true,
+			  captions: true,
+			  pagerSelector: '#pager',
+			  pagerType: 'short',
+			  auto: true,
+			  speed: 3000,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10
+			  
+			});
+
+    	});
